f0dcde8f44454b38dbb2b19fbf51f5427cd9fbaa
Pierre d'Herbemont <pdherbemont@free.fr>
2009-08-27 15:02:39 +0200

    DOMElement_Additions: Yet unknown reason to have an off by one. Might be the border. Here is a quick fix.

--- Sources/DOMElement_Additions.m
+++ Sources/DOMElement_Additions.m
@@ -21,6 +21,6 @@
 - (NSRect)frameInView:(NSView *)view
 {
     NSPoint origin = [self absolutePosition];
-    return NSMakeRect(origin.x + 1, [view frame].size.height - origin.y - self.clientHeight, self.clientWidth, self.clientHeight);
+    return NSMakeRect(origin.x + 1, [view frame].size.height - origin.y - self.clientHeight - 1, self.clientWidth, self.clientHeight);
 }
 @end
