f5d42ec9c32bd55deedb70c2fa0e1e02700fb420
Pierre d'Herbemont <pdherbemont@free.fr>
2009-08-29 19:30:17 +0200

    Build fixes.

--- Sources/VLCFullscreenController.m
+++ Sources/VLCFullscreenController.m
@@ -20,8 +20,8 @@
 
 #import "VLCFullscreenController.h"
 
-const NSInteger VLCFullscreenWindowLevel = NSFloatingWindowLevel
-const NSInteger VLCFullscreenHUDWindowLevel = NSFloatingWindowLevel + 1
+const NSInteger VLCFullscreenWindowLevel = 3; //NSFloatingWindowLevel
+const NSInteger VLCFullscreenHUDWindowLevel = 4; //NSFloatingWindowLevel + 1
 
 #import <Carbon/Carbon.h> // For SetSystemUIMode
 
