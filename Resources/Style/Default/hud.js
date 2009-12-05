dec25c7b335b53fefaa606e80f94fdb43c588e50
Pierre d'Herbemont <pdherbemont@free.fr>
2009-12-05 17:41:41 +0100

    Experimental is the new Default style.

--- Resources/Style/Default/hud.js
+++ Resources/Style/Default/hud.js
@@ -1,17 +1,61 @@
-var HUDController = new Object();
+var Experimental = new Object();
 
-HUDController.init = function() {
-    resetHudPosition();	
+Experimental.HUDController = new Object();
+
+Experimental.HUDController.init = function()
+{
+    window.windowController.navigationController.elementStyleUsesScrollBar = false;
+    Experimental.HUDController.resetHudPosition();
 }
 
-function resetHudPosition() {
-    var hud = document.getElementById('content');
+Experimental.HUDController.resetHudPosition =  function resetHudPosition()
+{
+    var hud = document.getElementById('draggable-controls');
     var marginBottom = 100;
     var bodyWidth = parseInt(document.body.clientWidth, 10);
     var bodyHeight = parseInt(document.body.clientHeight, 10);
     var hudWidth = parseInt(hud.clientWidth, 10);
     var hudHeight = parseInt(hud.clientHeight, 10);
-    
+
     hud.style.left = (bodyWidth - hudWidth) / 2 + 'px';
     hud.style.top = (bodyHeight - hudHeight - marginBottom) + 'px';
+}
+
+/* Playlist */
+var selectionIndex = 0;
+var timer = null;
+function removeCurrentSelection()
+{
+    var item = document.getElementById("tableItem" + selectionIndex);
+    item.removeClassName("selected");
+}
+
+var hideTimer = null;
+function hidePlaylist()
+{
+    var elmt = document.getElementById("more");
+    if (elmt)
+        elmt.addClassName("hidden");
+    hideTimer = null;
+}
+
+function onKeyDown(event)
+{
+    
+    var elmt = document.getElementById("more");
+    elmt.removeClassName("hidden");
+    
+    if (hideTimer)
+        window.clearTimeout(hideTimer);
+    
+    var handled = window.windowController.navigationController.currentView.keyDown(event);
+    switch (event.keyCode) {
+        case Lunettes.EventKey.Enter:
+            hidePlaylist();
+            break;
+        case Lunettes.EventKey.LeftArrow:
+            if (!handled)
+                hidePlaylist();
+            break;
+    }
 }
