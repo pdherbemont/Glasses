3517ac517f97c87a44c231eb000d56ce956356eb
Pierre d'Herbemont <pdherbemont@free.fr>
2009-12-11 16:06:23 +0100

    Always reset the WebKitInspector detached state and set it to NO.

--- Sources/VLCApplication.m
+++ Sources/VLCApplication.m
@@ -57,7 +57,16 @@
 
     /* register our default values... */
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:@"YES", @"ControlWithMediaKeys", @"YES", @"ControlWithMediaKeysInBackground", @"YES", @"ControlWithHIDRemote", @"YES", @"UseDeinterlaceFilter", @"~/Desktop", @"SelectedSnapshotFolder", nil]];
+    [defaults registerDefaults:[NSDictionary dictionaryWithObjectsAndKeys:
+                                @"YES", @"ControlWithMediaKeys",
+                                @"YES", @"ControlWithMediaKeysInBackground",
+                                @"YES", @"ControlWithHIDRemote",
+                                @"YES", @"UseDeinterlaceFilter",
+                                @"~/Desktop", @"SelectedSnapshotFolder", nil]];
+
+    // Always reset if the WebKitInspector was attached.
+    // Because its mostly unusable else.
+    [defaults setBool:NO forKey:@"WebKitInspectorAttached"];
 
     NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
     [center addObserver:self selector:@selector(coreChangedMediaKeySupportSetting:) name:@"NSUserDefaultsDidChangeNotification" object:nil];
