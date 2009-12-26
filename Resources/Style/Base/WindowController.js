150c0b4e9686ebd168e51b77042791b8c904268b
jason <jason.prado@gmail.com>
2009-12-25 20:01:35 -0600

    Check that a video is seekable before responding to click in timeline.
    This fixes #16, wherein setPosition was called on a video that was not
    seekable.
    
    The range input still responds to input when opacity:0, but we do nothing
    in the crashing case now.

--- Resources/Style/Base/WindowController.js
+++ Resources/Style/Base/WindowController.js
@@ -228,10 +228,12 @@ WindowController.prototype = {
      */        
     timelineValueChanged: function(event)
     {
-        var target = event.currentTarget;
-        window.PlatformView.setPosition_(target.value / target.getAttribute('max'));
+        if (window.PlatformView.isSeekable()) {
+            var target = event.currentTarget;
+            window.PlatformView.setPosition_(target.value / target.getAttribute('max'));
+        }
     },
-
+    
     /*************************************************
      * Window Drag
      */
