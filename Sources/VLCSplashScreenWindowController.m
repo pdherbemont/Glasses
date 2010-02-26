5f73aedce519f97a876f042e85dfe771365801da
Pierre d'Herbemont <pdherbemont@free.fr>
2010-02-26 15:46:05 +0100

    Revert "'freebox' is currently no valid discoverer"
    This reverts commit d98ca89951a9d7ccf4be0a378e3c1e7f9c237d9b.
    
    I think it's back in business, at least here. Tell me if you still have issue.

--- Sources/VLCSplashScreenWindowController.m
+++ Sources/VLCSplashScreenWindowController.m
@@ -62,6 +62,7 @@ static NSMutableArray * availableMediaDiscoverer = nil;     // Global list of me
         availableMediaDiscoverer = [[NSArray arrayWithObjects:
                                      [[[VLCMediaDiscoverer alloc] initWithName:@"sap"] autorelease],
                                      [[[VLCMediaDiscoverer alloc] initWithName:@"upnp_intel"] autorelease],
+                                     [[[VLCMediaDiscoverer alloc] initWithName:@"freebox"] autorelease],
                                      [[[VLCMediaDiscoverer alloc] initWithName:@"video_dir"] autorelease],
                                      [[[VLCDVDDiscoverer alloc] init] autorelease],nil] retain];
     }
